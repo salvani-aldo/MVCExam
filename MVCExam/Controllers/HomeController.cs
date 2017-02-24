b41f749f8350d609bd7dffdec8ea8af060d1a9c0
aldo.salvani <aldo.salvani@AGLS7281>
2017-02-24 16:35:31 +0800

    update

---
+++ MVCExam/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using MVCExam.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EmployeeInformationEntities db = new EmployeeInformationEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.EmployeeMasters.ToList());
+        }
+    }
+}
