8bc86ef8708543b0bbf78dbb3122b9ed32c40f1f
aldo.salvani <aldo.salvani@AGLS7281>
2017-02-26 02:15:30 +0800

    update

---
+++ MVCExam/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCExam.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private string _prefix = "echo: ";
+
+        public void Send(string message)
+        {
+            Clients.All.addNewMessage(_prefix + message);
+        }
+    }
+}
